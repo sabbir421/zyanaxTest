38538c8bb409d6c20d6f9b2385be74c0f5893e48
Sabbircoppanet <sabbir.a@coppanet.com>
2024-02-06 18:29:58 +0600

    checkout product

--- src/routes/product/productRoute.js
+++ src/routes/product/productRoute.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const multer = require("multer");
-const { addProduct, orderSummary } = require("../../controllers/productController");
+const {
+  addProduct,
+  orderSummary,
+  checkout,
+} = require("../../controllers/productController");
 
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 router.post("/add", upload.single("image"), addProduct);
-router.get("/summery",orderSummary)
+router.get("/summery", orderSummary);
+
+router.post("/checkout", checkout);
 
 module.exports = router;
